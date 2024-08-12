33ca0a3583c872c57717bfcda51a12d791ed3423
Halil777 <microsoft779@gmail.com>
2024-08-12 14:33:18 +0500

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./admin/Admin";
+import Login from "./Login";
+import AuthGuard from "./AuthGuard";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h2>Hello</h2>} />
-          <Route path="/admin/*" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/admin/*"
+          element={
+            <AuthGuard>
+              <Admin />
+            </AuthGuard>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
